74d8f7407d34c96a09fcfaa1405720ca917cc39f
randalllionelkharkrang <bahduh94@gmail.com>
2025-03-17 15:07:53 +0530

    Added SpinLock Implementation

---
+++ spinlock/examples/counter.rs
@@ -0,0 +1,16 @@
+use std::thread;
+use spinlock::SpinLock;  // Import from your library
+
+fn main() {
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
